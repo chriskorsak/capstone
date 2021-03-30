d61fb40f0243e14c2894da85579ba0b2eb6a496f
chriskorsak <chris@chriskorsak.com>
2021-03-30 13:01:29 -0700

    dismiss welcome message on index page logic: use local storage to remember if user has dismissed welcome to never show it again if user isn't logged in.

--- ckblues/static/ckblues/scripts2.js
+++ ckblues/static/ckblues/scripts2.js
@@ -1,13 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // add click event to dimiss welcome link
-  let dismiss = document.querySelector('#dismiss');
-    dismiss.addEventListener('click', function() {
-      //run function when dismiss clicked
-      dismissWelcome(dismiss);
-    });
-  });
-
-function dismissWelcome(dismiss) {
-  document.querySelector("#welcome").style.display = 'none';
-  
+// create 'firstVisit' and set to 0 if not present
+if (!localStorage.getItem('firstVisit')) {
+  localStorage.setItem('firstVisit', 0);
 }
+
+// allow user who isn't logged in to dismiss welcome message at top of index page and not reappear
+document.addEventListener("DOMContentLoaded", function() {
+  let dismiss = document.querySelector('#dismiss');
+    //dismiss element will be present on page to add click event
+    if (dismiss !== null) {
+      let firstVisit = localStorage.getItem('firstVisit');
+      if (firstVisit > 0) {
+        document.querySelector("#welcome").style.display = 'none';
+      }
+      // add click event to dismiss
+      dismiss.addEventListener('click', function() {
+        // increment firstVisit so user won't see welcome again in future
+        localStorage.setItem('firstVisit', 1);
+        // hide welcome after dissmissing
+        document.querySelector("#welcome").style.display = 'none';
+      });
+    }
+  });
